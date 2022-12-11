057aec996871e3693434d0f1dfc48dadf7d49f5d
Masulka <kuzmenkomkbk@gmail.com>
2022-12-11 10:09:43 +0300

    task 1 finished

--- Sem6_1HARD/Program.cs
+++ Sem6_1HARD/Program.cs
@@ -1,55 +1,55 @@
-﻿// Сгенерировать массив случайных целых чисел размерностью m*n (размерность вводим с клавиатуры). 
-// Вывести на экран красивенько таблицей. 
-// Найти минимальное число и его индекс, найти максимальное число и его индекс. 
+﻿// Сгенерировать массив случайных целых чисел размерностью m*n (размерность вводим с клавиатуры).
+// Вывести на экран красивенько таблицей.
+// Найти минимальное число и его индекс, найти максимальное число и его индекс.
 // Вывести эту информацию на экран.
 
-int [,]FillAndShowArray(int stroka, int stolbec)
+int[,] FillAndShowArray(int stroka, int stolbec)
 {
-    int [,]Array = new int[stroka,stolbec];
-    for (int i=0;i<stolbec; i++)
+    int[,] Array = new int[stroka, stolbec];
+    for (int i = 0; i < stolbec; i++)
     {
-        for (int j=0; j<stroka;j++)
+        for (int j = 0; j < stroka; j++)
         {
-        Array[j,i] = new Random().Next(1,10);
-        Console.Write($"{Array[j,i]} ");
+            Array[j, i] = new Random().Next(1, 10);
+            Console.Write($"{Array[j, i]} ");
         }
         Console.WriteLine();
     }
     return Array;
 }
-void FindMinMax(int[,]arr)
+void FindMinMax(int[,] arr)
 {
-    int min = arr[0,0];
-    int max = arr[0,0];
+    int min = arr[0, 0];
+    int max = arr[0, 0];
     int minj = 0;
     int mini = 0;
     int maxj = 0;
     int maxi = 0;
-    for (int i=0;i<arr.GetLength(1); i++)
+    for (int i = 0; i < arr.GetLength(1); i++)
     {
-        for (int j=0; j<arr.GetLength(0);j++)
+        for (int j = 0; j < arr.GetLength(0); j++)
         {
-            if(arr[j,i]<min) 
+            if (arr[j, i] < min)
             {
-                min = arr[j,i];
+                min = arr[j, i];
                 minj = j;
                 mini = i;
             }
-            if(arr[j,i]>max) 
+            if (arr[j, i] > max)
             {
-                max = arr[j,i];
+                max = arr[j, i];
                 maxj = j;
                 maxi = i;
             }
         }
     }
-Console.WriteLine($"Минимальное число массива равно: {min}, расположено на {mini+1} строке, в {minj+1} столбце");
-        Console.WriteLine($"Максимальное число массива равно: {max}, расположено на {maxi+1} строке, в {maxj+1} столбце");
+    Console.WriteLine($"Минимальное число массива равно: {min}, расположено на {mini + 1} строке, в {minj + 1} столбце");
+    Console.WriteLine($"Максимальное число массива равно: {max}, расположено на {maxi + 1} строке, в {maxj + 1} столбце");
 }
 
 Console.WriteLine("Введите количество строк в таблице");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов в таблице");
 int n = Convert.ToInt32(Console.ReadLine());
-int [,] arr = FillAndShowArray(m,n);
+int[,] arr = FillAndShowArray(m, n);
 FindMinMax(arr);
