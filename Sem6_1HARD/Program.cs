e2360a33cae84c51edb54863c97dc563d98717bd
Masulka <kuzmenkomkbk@gmail.com>
2022-12-11 10:00:16 +0300

    task 1 started

--- Sem6_1HARD/Program.cs
+++ Sem6_1HARD/Program.cs
@@ -2,3 +2,54 @@
 // Вывести на экран красивенько таблицей. 
 // Найти минимальное число и его индекс, найти максимальное число и его индекс. 
 // Вывести эту информацию на экран.
+
+int [,]FillAndShowArray(int stroka, int stolbec)
+{
+    int [,]Array = new int[stroka,stolbec];
+    for (int i=0;i<stolbec; i++)
+    {
+        for (int j=0; j<stroka;j++)
+        {
+        Array[j,i] = new Random().Next(1,10);
+        Console.Write($"{Array[j,i]} ");
+        }
+        Console.WriteLine();
+    }
+    return Array;
+}
+void FindMinMax(int[,]arr)
+{
+    int min = arr[0,0];
+    int max = arr[0,0];
+    int minj = 0;
+    int mini = 0;
+    int maxj = 0;
+    int maxi = 0;
+    for (int i=0;i<arr.GetLength(1); i++)
+    {
+        for (int j=0; j<arr.GetLength(0);j++)
+        {
+            if(arr[j,i]<min) 
+            {
+                min = arr[j,i];
+                minj = j;
+                mini = i;
+            }
+            if(arr[j,i]>max) 
+            {
+                max = arr[j,i];
+                maxj = j;
+                maxi = i;
+            }
+        }
+    }
+Console.WriteLine($"Минимальное число массива равно: {min}, расположено на {minj} строке, в {mini} столбце");
+        Console.WriteLine($"Максимальное число массива равно: {max}, расположено на {maxj} строке, в {maxi} столбце");
+}
+
+Console.WriteLine("Введите количество строк в таблице");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в таблице");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] arr = FillAndShowArray(m,n);
+FindMinMax(arr);
